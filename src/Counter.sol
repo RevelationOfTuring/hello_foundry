1f1ead584aaa1e285dd04c5f20a4bb1aeeed3a7d
MichaelWang <michael_wangguanyi@163.com>
2023-06-13 14:17:59 +0800

    remappings

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+//import "solmate/tokens/ERC20.sol";
+import "solmate/utils/CREATE3.sol";
+
 contract Counter {
+    using CREATE3 for bytes32;
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
