47ffc64495efb4aaad7948c78e9343c72c1aeba9
MichaelWang <michael_wangguanyi@163.com>
2023-06-15 13:56:30 +0800

    upd

--- test/Cheatcode.t.sol
+++ test/Cheatcode.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 
+    error ErrorCustomized();
 contract Demo {
     uint public varSlot0 = 1;
     // private
@@ -15,9 +16,41 @@ contract Demo {
         msgSender = msg.sender;
         txOrigin = tx.origin;
     }
+
+    function Revert(uint flag) pure external {
+        require(flag > 0, "require revert msg");
+        if (flag == 1) {
+            revert();
+        } else if (flag == 2) {
+            revert ErrorCustomized();
+        } else {
+            revert("revert msg");
+        }
+    }
 }
 
 contract Cheatcode is Test {
+
+    function test_ExpectRevert() external {
+        Demo demo = new Demo();
+        // check require revert msg
+        vm.expectRevert("require revert msg");
+        demo.Revert(0);
+
+        // check revert
+        vm.expectRevert();
+        demo.Revert(1);
+
+        // check revert with error
+        vm.expectRevert(ErrorCustomized.selector);
+        demo.Revert(2);
+
+        // check revert with msg
+        vm.expectRevert("revert msg");
+        demo.Revert(3);
+
+    }
+
     function test_Etch() external {
         // 零地址的codehash和code都没有值
         assertEq(address(0).codehash, bytes32(uint(0)));
