f5afd341a3e824bef4dfb9e0487f0c6c2c35b996
MichaelWang <michael_wangguanyi@163.com>
2023-06-15 12:34:10 +0800

    upd

--- test/Cheatcode.t.sol
+++ test/Cheatcode.t.sol
@@ -8,9 +8,91 @@ contract Demo {
     uint public varSlot0 = 1;
     // private
     uint  varSlot1 = 2;
+    address public msgSender;
+    address public txOrigin;
+
+    function testPrank() external {
+        msgSender = msg.sender;
+        txOrigin = tx.origin;
+    }
 }
 
 contract Cheatcode is Test {
+    function test_Etch() external {
+        // 零地址的codehash和code都没有值
+        assertEq(address(0).codehash, bytes32(uint(0)));
+        assertEq(address(0).code, bytes(""));
+
+        // 设置目标地址下的合约bytecode(设置成Demo的runtime code)
+        vm.etch(address(0), type(Demo).runtimeCode);
+        // 调用0地址的合约
+        Demo(address(0)).testPrank();
+        // 验证调用成功，说明runtime code已经被成功替换
+        assertEq(Demo(address(0)).msgSender(), address(this));
+        assertEq(Demo(address(0)).txOrigin(), msg.sender);
+
+        // 零地址的codehash和code此时已经有值
+        assertNotEq(address(0).codehash, bytes32(uint(0)));
+        assertNotEq0(address(0).code, bytes(""));
+    }
+
+    function test_Deal() external {
+        // 设置某地址下的eth余额
+        assertEq(address(0).balance, 0);
+        vm.deal(address(0), 1e18 gwei);
+        assertEq(address(0).balance, 1e18 gwei);
+    }
+
+    function test_StartAndStopPrank() external {
+        Demo demo = new Demo();
+        // 在startPrank和stopPrank之间的所有call的msg.sender都保持改变
+        vm.startPrank(address(1));
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(1));
+        assertEq(demo.txOrigin(), msg.sender);
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(1));
+        assertEq(demo.txOrigin(), msg.sender);
+
+        // 在startPrank和stopPrank之间的所有call的msg.sender和tx.origin都保持改变
+        vm.startPrank(address(2), address(2));
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(2));
+        assertEq(demo.txOrigin(), address(2));
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(2));
+        assertEq(demo.txOrigin(), address(2));
+
+        // stopPrank会恢复正常的逻辑
+        vm.stopPrank();
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(this));
+        assertEq(demo.txOrigin(), msg.sender);
+    }
+
+    function test_Prank() external {
+        Demo demo = new Demo();
+        // 只设置下一个call的msg.sender
+        vm.prank(address(1));
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(1));
+        assertEq(demo.txOrigin(), msg.sender);
+        // 再下一个call将恢复原样
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(this));
+        assertEq(demo.txOrigin(), msg.sender);
+
+        // 设置下一个call的msg.sender和tx.origin
+        vm.prank(address(2), address(2));
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(2));
+        assertEq(demo.txOrigin(), address(2));
+        // 再下一个call将恢复原样
+        demo.testPrank();
+        assertEq(demo.msgSender(), address(this));
+        assertEq(demo.txOrigin(), msg.sender);
+    }
+
     function test_SetAndGetNonce() external {
         assertEq(vm.getNonce(address(0)), 0);
         vm.setNonce(address(0), 1024);
